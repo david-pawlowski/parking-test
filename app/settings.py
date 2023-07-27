17f9f568785942c50d2a464e2c9b141fed1f55af
dawid-pawlowski <dawid.pawlowski@zielonykod.com>
2023-07-27 23:54:25 +0200

    Add azure to allowed hosts

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-o#l%po=@7zk!&90ip8^lr93r&b2z=q#8-g#wkba9rwlbyzkv5_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['parkinguy.azurewebsites.net',]
 
 
 # Application definition
@@ -72,7 +72,6 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -83,7 +82,6 @@ DATABASES = {
         'PORT': '5432',  # Default PostgreSQL port
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
