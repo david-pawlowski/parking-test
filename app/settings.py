eeebb200a8b0b8ea4754b51f15bbfb96e3185fbf
dawid-pawlowski <dawid.pawlowski@zielonykod.com>
2023-08-17 02:32:21 +0200

    Add swagger for docs

--- app/settings.py
+++ app/settings.py
@@ -66,8 +66,8 @@ ROOT_URLCONF = "app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
+        "DIRS": ["templates"],
+        "APP_DIRS": False,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import include, path
+from rest_framework.schemas import get_schema_view
+
 from parking.urls import router
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include(router.urls))]
+urlpatterns = [
+    path('api_schema/', get_schema_view(
+        title='API Schema',
+        description='Guide for the REST API'
+    ), name='schema_url'),
+    path('docs', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'schema_url'}
+    ), name='swagger-ui'),
+    path("admin/", admin.site.urls),
+    path("", include(router.urls))
+]
