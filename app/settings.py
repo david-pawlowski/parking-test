4cb58ed8d81792641140ce4d574f7dd1a1832c86
dawid-pawlowski <dawid.pawlowski@zielonykod.com>
2023-08-19 14:50:55 +0200

    Black on new files

--- app/settings.py
+++ app/settings.py
@@ -146,4 +146,6 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = "accounts.User"
 
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://127.0.0.1:6379/0")
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://127.0.0.1:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get(
+    "CELERY_BACKEND", "redis://127.0.0.1:6379/0"
+)
