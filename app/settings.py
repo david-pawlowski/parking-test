40acdd3b63c2d52658b6a6a2a1232647cc56392b
dawid-pawlowski <dawid.pawlowski@zielonykod.com>
2023-08-19 16:00:50 +0200

    Black

--- app/settings.py
+++ app/settings.py
@@ -156,4 +156,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = '<parking-zielony@noreply.com>'
+DEFAULT_FROM_EMAIL = "<parking-zielony@noreply.com>"
--- parking/views.py
+++ parking/views.py
@@ -14,6 +14,7 @@ from parking.serializers import (
 )
 from parking.tasks import send_reservation_mail
 
+
 class ParkingViewSet(viewsets.ModelViewSet):
     queryset = ParkingModel.objects.all()
     serializer_class = ParkingSerializer
@@ -38,7 +39,9 @@ class ReservationViewSet(viewsets.ModelViewSet):
         # to check when reservation time ends
         # parking spot sensor data to determine if it is occupied
         # If it is start charging extra
-        user_email = User.objects.get(pk=serializer.validated_data.get('reserved_by')).email
+        user_email = User.objects.get(
+            pk=serializer.validated_data.get("reserved_by")
+        ).email
         email_content = "leno paleno"
         send_reservation_mail.delay(user_email, email_content)
         parking_spot.occupied = True
