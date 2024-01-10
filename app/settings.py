8571ebfde2a95dd7584676c2689f474a314c97ef
dawid-pawlowski <dawid.pawlowski@zielonykod.com>
2024-01-11 00:31:12 +0100

    Change permissions to readonly

--- app/settings.py
+++ app/settings.py
@@ -146,6 +146,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ),
 }
 
 AUTH_USER_MODEL = "accounts.User"
